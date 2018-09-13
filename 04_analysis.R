ea015ea9e620fd317b5cd8e5976a9f96d4495ce4
Belen Rodriguez <brodriguez@wesleyan.edu>
2018-09-13 15:43:15 -0400

    added prediction variables

--- 04_analysis.R
+++ 04_analysis.R
@@ -1,5 +1,6 @@
 # linear prediction of salary
-fit.lm <- lm(salary ~ rank + yrs.since.phd +
+fit.lm <- lm(salary ~ rank + yrs.since.phd + yrs.service +
 	discipline + sex, data = Salaries) #sex still explains differences – would highlight gender pay gap if there is one
 
 summary(fit.lm)
+plot(fit.lm)
